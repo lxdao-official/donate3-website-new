4badf9b51ef1698b7afa5f47d2aa83d68d5c05d5
0xhardman <1142783160@qq.com>
2023-08-11 20:03:35 +0800

    change color

--- utils/const.ts
+++ utils/const.ts
@@ -3,7 +3,7 @@ export const DONATE_SDK_LATEST_VERSION = '1.0.10';
 export const DONATE_SDK_URL = `https://cdn.jsdelivr.net/npm/donate3-sdk@${DONATE_SDK_LATEST_VERSION}/dist/webpack/bundle.js`;
 
 export const DEFAULT_CREATE_NAME = 'Donate3';
-export const DEFAULT_CREATE_COLOR = '#396AFF';
+export const DEFAULT_CREATE_COLOR = '#b9e927';
 export const DEFAULT_CREATE_ADDRESS = '0xe395B9bA2F93236489ac953146485C435D1A267B';
 
 export enum AccountType {
