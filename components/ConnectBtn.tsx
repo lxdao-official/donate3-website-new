c84cf52ba74724b521bc270934b6b2f1b20d9c23
TwilightLogic <twilightlogic@gmail.com>
2023-09-25 19:23:31 +0800

    Change "select wallet' style

--- components/ConnectBtn.tsx
+++ components/ConnectBtn.tsx
@@ -6,7 +6,7 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
-import '@solana/wallet-adapter-react-ui/styles.css';
+import '@solana/wallet-adapter-react-ui/override-styles.css';
 
 const ConnectBtn: FC = () => {
   const network = WalletAdapterNetwork.Devnet;
