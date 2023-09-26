e07366e490cf5e33e545e395f2acd86f83354ffe
TwilightLogic <twilightlogic@gmail.com>
2023-09-26 11:32:00 +0800

    Update the ConnectBtn style

--- components/ConnectBtn.tsx
+++ components/ConnectBtn.tsx
@@ -6,7 +6,9 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
-import '@solana/wallet-adapter-react-ui/styles.css';
+
+// import '@solana/wallet-adapter-react-ui/styles.css';
+// import '@/styles/solanaWallet.css';
 
 const ConnectBtn: FC = () => {
   const network = WalletAdapterNetwork.Devnet;
@@ -17,7 +19,9 @@ const ConnectBtn: FC = () => {
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          <WalletMultiButton />
+          <div className="view">
+            <WalletMultiButton />
+          </div>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
