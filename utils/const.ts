b58964f12e0f31bfc3152ee78f7d30c1f3181caf
0xhardman <1142783160@qq.com>
2023-10-30 15:28:11 +0800

    update

--- utils/const.ts
+++ utils/const.ts
@@ -1,4 +1,4 @@
-export const DONATE_SDK_LATEST_VERSION = '1.0.37';
+export const DONATE_SDK_LATEST_VERSION = '1.0.43';
 
 export const DONATE_SDK_URL = `https://cdn.jsdelivr.net/npm/donate3-sdk@${DONATE_SDK_LATEST_VERSION}/dist/webpack/bundle.js`;
 
